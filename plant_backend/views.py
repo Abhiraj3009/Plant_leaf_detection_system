63965b3da909aa98aa5d2b7b7f96345df8c27754
Abhiraj Pathak <abhiraj.pathak3009@gmail.com>
2026-05-19 13:08:41 +0530

    small change in views.py to comment out the normalization step for testing purposes.

--- plant_backend/views.py
+++ plant_backend/views.py
@@ -45,7 +45,7 @@ def test_prediction_view(request):
             img = tf.keras.utils.load_img(file_path, target_size=(224, 224))
             img_array = tf.keras.utils.img_to_array(img)
             img_array = np.expand_dims(img_array, axis=0)  # Make batch shape (1, 224, 224, 3)
-            img_array = img_array / 255.0                  # Rescale normalization
+            #img_array = img_array / 255.0                  # Rescale normalization
             
             # 3. Run model prediction
             if model:
